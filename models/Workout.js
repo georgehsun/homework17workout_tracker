a1f5476284c97e1ba9366bb4354c954c7312d899
George Sun <Gsunems@gmail.com>
2020-04-12 18:52:12 -0500

    uploading my schemas

---
+++ models/Workout.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [{
+        name: {
+            type: String,
+            trim: true,
+            required: "Name is Required"
+        },
+
+        type: {
+            type: String,
+            trim: true,
+            required: "Enter a exercise type"
+        },
+
+        weight: {
+            type: Number,
+        },
+
+        sets: {
+            type: Number,
+        },
+
+        reps: {
+            type: Number,
+        },
+
+        duration: {
+            type: Number,
+            required: "how long did the workout take you"
+        },
+
+        distance: {
+            type: Number,
+        },
+    }]
+
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
